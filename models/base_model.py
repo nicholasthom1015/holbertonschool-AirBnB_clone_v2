c8aaeb505f0d994fb21fe09a1795cb672ee49a9b
daeshov <jdarahthomas@gmail.com>
2023-03-07 01:38:22 +0000

    task 6

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,10 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid.uuid4()for i in range(60))
+
             self.created_at = datetime.now()
+            
             self.updated_at = datetime.now()
             storage.new(self)
         else:
