d713639462fb1d957e88e89083655bf6fd0f128d
daeshov <jdarahthomas@gmail.com>
2023-03-08 17:42:15 +0000

    task 7

--- models/user.py
+++ models/user.py
@@ -22,7 +22,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade='all, delete, delete-orphan')
-    reviews = relationship("Review", backref="user", cascade='all, delete, delete-orphan')
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user', cascade='all, delete, delete-orphan')
+    reviews = relationship('Review', backref='user', cascade='all, delete, delete-orphan')
