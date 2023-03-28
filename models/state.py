52ff643ddd08a9c6aae843135ec837036714ad41
root <jdarahthomas@gmail.com>
2023-03-28 18:39:52 +0000

    task 7

--- models/state.py
+++ models/state.py
@@ -20,7 +20,7 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
+
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
