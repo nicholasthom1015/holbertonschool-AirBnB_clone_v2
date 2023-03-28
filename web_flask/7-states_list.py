32322e903597b67ee263dc2dc270186f899d9a4f
root <jdarahthomas@gmail.com>
2023-03-28 18:49:30 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,74 +18,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """display text"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb')
-def hbnb():
-    """display text"""
-    return "HBNB"
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """display custom text given"""
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route('/python')
-@app.route('/python/<text>')
-def python_text(text="is cool"):
-    """display custom text given
-       first route statement ensures it works for:
-          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
-          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
-    """
-    return "Python {}".format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def text_if_int(n):
-    """display text only if int given"""
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_if_int(n):
-    """display html page only if int given
-       place given int into html template
-    """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def html_odd_or_even(n):
-    """display html page only if int given
-       place given int into html template
-       substitute text to display if int is odd or even
-    """
-    odd_or_even = "even" if (n % 2 == 0) else "odd"
-    return render_template('6-number_odd_or_even.html',
-                           n=n, odd_or_even=odd_or_even)
+@app.route('/states_list/', strict_slashes=False)
+def listthem():
+    """88888"""
+    states = storage.all('State')
+    render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """after each request remove current SQLAlchemy session"""
+def storclo():
+    """8 agaain"""
     storage.close()
-
-
-@app.route('/states_list')
-def html_fetch_states():
-    """display html page
-       fetch sorted states to insert into html in UL tag
-    """
-    state_objs = [s for s in storage.all("State").values()]
-    return render_template('7-states_list.html',
-                           state_objs=state_objs)
 
 
 if __name__ == "__main__":
